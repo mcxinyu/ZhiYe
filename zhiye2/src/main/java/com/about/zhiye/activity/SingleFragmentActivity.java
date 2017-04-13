e35c73e219b457c2bf61c941377809792a483f3e
Vicent <mcxinyu@foxmail.com>
2017-04-13 22:47:09 +0800

    知也的第二版本！ 抽象 SingleFragmentActivity

---
+++ zhiye2/src/main/java/com/about/zhiye/activity/SingleFragmentActivity.java
@@ -0,0 +1,70 @@
+package com.about.zhiye.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.about.zhiye.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by huangyuefeng on 2017/4/13.
+ * Contact me : mcxinyu@foxmail.com
+ */
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.fragment_container)
+    FrameLayout mFragmentContainer;
+
+    protected abstract Fragment createFragment();
+    protected abstract boolean setHaveToolbar();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_fragment);
+        ButterKnife.bind(this);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+        if (fragment == null) {
+            fragment = createFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+
+        if (setHaveToolbar()){
+            initToolbar();
+        }
+    }
+
+    private void initToolbar() {
+        setSupportActionBar(mToolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
