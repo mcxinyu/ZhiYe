d4e081fa5dd61daed0c0783293de63d152c2d329
Vicent <mcxinyu@foxmail.com>
2017-04-16 17:01:54 +0800

    知也的第二版本！ 抽象 BaseActivity

---
+++ zhiye2/src/main/java/com/about/zhiye/activity/BaseActivity.java
@@ -0,0 +1,45 @@
+package com.about.zhiye.activity;
+
+import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.about.zhiye.R;
+
+/**
+ * Created by huangyuefeng on 2017/4/15.
+ * Contact me : mcxinyu@foxmail.com
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
+
+    @LayoutRes
+    protected int getContentViewId() {
+        return R.layout.activity_frame_layout;
+    }
+
+    @IdRes
+    protected int getFragmentContentId() {
+        return R.id.fragment_container;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getContentViewId());
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(getFragmentContentId());
+        if (fragment == null) {
+            fragment = createFragment();
+            fragmentManager.beginTransaction()
+                    .add(getFragmentContentId(), fragment)
+                    .commit();
+        }
+    }
+}
--- zhiye2/src/main/java/com/about/zhiye/activity/SingleFragmentActivity.java
+++ zhiye2/src/main/java/com/about/zhiye/activity/SingleFragmentActivity.java
@@ -3,10 +3,7 @@ package com.about.zhiye.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
@@ -20,7 +17,7 @@ import butterknife.ButterKnife;
  * Created by huangyuefeng on 2017/4/13.
  * Contact me : mcxinyu@foxmail.com
  */
-public abstract class SingleFragmentActivity extends AppCompatActivity {
+public abstract class SingleFragmentActivity extends BaseActivity {
 
     @BindView(R.id.app_bar_layout)
     AppBarLayout mAppBarLayout;
@@ -29,24 +26,22 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
     @BindView(R.id.fragment_container)
     FrameLayout mFragmentContainer;
 
-    protected abstract Fragment createFragment();
-
     protected abstract boolean setHasToolbar();
+
+    @Override
+    protected int getContentViewId() {
+        return R.layout.activity_single_fragment;
+    }
+
+    @Override
+    protected int getFragmentContentId() {
+        return super.getFragmentContentId();
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_single_fragment);
         ButterKnife.bind(this);
-
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
-        if (fragment == null) {
-            fragment = createFragment();
-            fragmentManager.beginTransaction()
-                    .add(R.id.fragment_container, fragment)
-                    .commit();
-        }
 
         if (setHasToolbar()) {
             initToolbar();
